7474abd3f042ecfd364f6889800221d2dc19338b
richie17777 <asa21006@byui.edu>
2022-02-02 15:35:41 +0000

    initial commit for week four assignment

---
+++ lesson04/js/date.js
@@ -0,0 +1,7 @@
+// const datefield = document.querySelector("date");
+// const now = new Date();
+// const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+// datefield.innerHTML = `<div>${fulldate}</div>`;
+
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last updated: ${document.lastModified}`;
