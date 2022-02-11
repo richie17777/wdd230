1615c6078a87280c0521afd00790771b11bc7d55
richie17777 <asa21006@byui.edu>
2022-02-11 23:11:45 +0000

    Adding assignment for bom.js

--- lesson5/bom.js
+++ lesson5/bom.js
@@ -1,13 +1,23 @@
-const output = document.querySelector("#favchap");
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
 const button = document.querySelector("button");
-const theList = document.querySelector("#list");
 
-button.addEventListener("click", () => {
-  let userInput = output.value;
-  for (let i = 1; i <= userInput; i++){
-    let li = document.createElement("li");
-    li.textContent = `Item: ${i}`;
-    theList.appendChild(li);
+button.addEventListener('click', function() { 
+  let myBooks = input.value;
+  input.value = " ";
 
-  }
-});
+  const listItem = document.createElement("li");
+  const listText = document.createElement("span")
+  const listBtn = document.createElement("button");
+
+  listItem.appendChild(listText);
+  listText.innerHTML = myBooks
+  listItem.appendChild(listBtn);
+  listBtn.innerHTML = "X";
+  list.appendChild(listItem);
+
+  listBtn.onclick = function(e) {
+  list.removeChild(listItem);
+  };
+  input.focus();
+})
