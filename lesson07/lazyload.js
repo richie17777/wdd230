296771a86a7513d6387bbcba41cb1c4505a606b5
richie17777 <asa21006@byui.edu>
2022-02-24 21:29:31 +0000

    Minor changes

--- lesson07/lazyload.js
+++ lesson07/lazyload.js
@@ -1,16 +1,22 @@
 
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
   threshold: 0,
   rootmargin: "0px 0px 50px 0px",
 };
 
-
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {image.removeAttribute('data-src');};
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
+
+// const loadImages = (image) => {
+//   image.setAttribute('src', image.getAttribute('data-src'));
+//   image.onload = () => {image.removeAttribute('data-src');};
+// };
 
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
