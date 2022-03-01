012091ae668b4dddd7b5791f2f4cdba1e8242b2b
richie17777 <asa21006@byui.edu>
2022-03-01 23:35:18 +0000

    Added changes to discover page

--- Chambers/js/dates.js
+++ Chambers/js/dates.js
@@ -1,2 +1,18 @@
-const lastmod = document.querySelector('#lastmod');
-lastmod.textContent = `Last updated: ${document.lastModified}`;
+
+// const lastmod = document.querySelector('#lastmod');
+// lastmod.textContent = `Last updated: ${document.lastModified}`;
+
+// Displayin the number of visitsto the page
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits;
+} else {
+  visitsDisplay.textContent = `This is your first visit!`;
+}
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
+
+todayDisplay.textContent = Date.now();
