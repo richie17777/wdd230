46adcabd2948eeadc8c15559f997d089ccc8dfdf
richie17777 <asa21006@byui.edu>
2022-03-08 00:19:30 +0000

    Added prohets.htm file for week 9 assignment

---
+++ lesson09/prophets.js
@@ -0,0 +1,58 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+
+  });
+
+function displayProphets(prophet) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthdate = document.createElement('h3')
+    let birthplace = document.createElement('h4')
+    let portrait = document.createElement('img');
+    let order = ``;
+
+    switch(prophet.order){
+      case 1:
+        order = `${prophet.order}st`;
+        break;
+      case 2:
+        order = `${prophet.order}nd`;
+        break;
+      case 3:
+        order = `${prophet.order}rd`;
+        break;
+      default:
+        order = `${prophet.order}th`;
+    }
+  
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+    birthdate.innerHTML = ` Date of birth: ${prophet.birthdate}`;
+    birthplace.innerHTML = `Place of birth: ${prophet.birthPlace}`;
+
+  
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} ${order} Latter-day President`);
+    
+    portrait.setAttribute('loading', 'lazy');
+  
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(birthdate)
+    card.appendChild(birthplace)
+    card.appendChild(portrait);
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    cards.appendChild(card);
+}
