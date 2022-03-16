38689da5815ae9a5530bb934ef4b33e308fc7a48
richie17777 <asa21006@byui.edu>
2022-03-16 00:12:52 +0000

    Added additional stuff

--- Chambers/js/windchill.js
+++ Chambers/js/windchill.js
@@ -4,8 +4,9 @@ fetch(temp)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.querySelector('#temp').textContent = jsObject.main.temp.toFixed(1);
-    document.querySelector('#windSpeed').textContent = jsObject.wind.speed.toFixed(1);
+
+    const temp = document.querySelector('#temp'); //#endregion.textContent = jsObject.main.temp.toFixed(1);
+    const wspeed = document.querySelector('#windSpeed'); //#endregion.textContent = jsObject.wind.speed.toFixed(1);
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
@@ -13,19 +14,22 @@ fetch(temp)
     document.querySelector('#weathericon').setAttribute('alt', desc);
     document.querySelector('figcaption').innerHTML = desc;
 
+    temp.innerHTML = jsObject.main.temp.toFixed(1);
+    wspeed.innerHTML = jsObject.wind.speed.toFixed(1);
+
     let windchill = ""
 
-    if (temp <= 50 && wsp > 3){
-      windchill = windChill(temp, wsp);
+    if (temp <= 50 && wspeed > 3){
+      windchill = windChill(temp, wspeed);
       windchill = `${windchill}`;
     }else{
       windChill = "N/A";
     }
     
-    document.querySelector('#chill').textContent = windChill;
+    document.querySelector('#chill').innerHTML = windchill;
     
     function windChill(t,s){
-      return (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+      return (35.74 + (0.6215 * t))-(35.75 * Math.pow(s,0.16)) + (0.4275*t*Math.pow(s,0.16));
     }
   });
 
